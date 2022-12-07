56863d88a1222d42764a3b63195cf857835dd496
cmyui <cmyuiosu@gmail.com>
2022-12-07 01:08:49 -0500

    refactor: remove duplicated code

--- noapi/__main__.py
+++ noapi/__main__.py
@@ -24,44 +24,28 @@ def get_http_method(method: controllers.Method) -> str:
 
 def create_endpoint(
     resource_name: str,
-    resource: dict[str, Any],
     method: controllers.Method,
-    model: models.BaseModel,
+    model: type[models.BaseModel],
 ) -> APIRoute:
     if method == controllers.Method.GET_ONE:
-        endpoint_function = controllers.create_get_one_function(
-            resource_name,
-            resource["model"],
-        )
+        endpoint_function = controllers.create_get_one_function(resource_name, model)
         path = f"/{resource_name.lower()}/{{id}}"
         response_model = model
     elif method == controllers.Method.GET_MANY:
-        endpoint_function = controllers.create_get_many_function(
-            resource_name,
-            resource["model"],
-        )
+        endpoint_function = controllers.create_get_many_function(resource_name, model)
         path = f"/{resource_name.lower()}"
-        response_model = list[type(model)]  # TODO: does this work? lol
+        response_model = list[type[model]]  # TODO: does this work? lol
         print(response_model)
     elif method == controllers.Method.POST:
-        endpoint_function = controllers.create_post_function(
-            resource_name,
-            resource["model"],
-        )
+        endpoint_function = controllers.create_post_function(resource_name, model)
         path = f"/{resource_name.lower()}"
         response_model = model
     elif method == controllers.Method.PATCH:
-        endpoint_function = controllers.create_patch_function(
-            resource_name,
-            resource["model"],
-        )
+        endpoint_function = controllers.create_patch_function(resource_name, model)
         path = f"/{resource_name.lower()}/{{id}}"
         response_model = model
     elif method == controllers.Method.DELETE:
-        endpoint_function = controllers.create_delete_function(
-            resource_name,
-            resource["model"],
-        )
+        endpoint_function = controllers.create_delete_function(resource_name, model)
         path = f"/{resource_name.lower()}/{{id}}"
         response_model = model
     else:
@@ -81,61 +65,19 @@ def create_endpoint(
 def main(resources: Mapping[str, Any]) -> int:
     routes: list[starlette.routing.BaseRoute] = []
 
-    for resource, resource_def in resources.items():
+    for resource_name, resource in resources.items():
         resource_model = pydantic.create_model(
-            resource,
-            **resource_def["model"],
+            resource_name,
+            **resource["model"],
             __base__=models.BaseModel,
         )
 
-        for method in resource_def["methods"]:
-            if method == controllers.Method.GET_ONE:
-                endpoint_function = controllers.create_get_one_function(
-                    resource,
-                    resource_model,
-                )
-                path = f"/{resource.lower()}/{{id}}"
-                response_model = resource_model
-            elif method == controllers.Method.GET_MANY:
-                endpoint_function = controllers.create_get_many_function(
-                    resource,
-                    resource_model,
-                )
-                path = f"/{resource.lower()}"
-                response_model = list[type[resource_model]]
-            elif method == controllers.Method.POST:
-                endpoint_function = controllers.create_post_function(
-                    resource,
-                    resource_model,
-                )
-                path = f"/{resource.lower()}"
-                response_model = resource_model
-            elif method == controllers.Method.PATCH:
-                endpoint_function = controllers.create_patch_function(
-                    resource,
-                    resource_model,
-                )
-                path = f"/{resource.lower()}/{{id}}"
-                response_model = resource_model
-            elif method == controllers.Method.DELETE:
-                endpoint_function = controllers.create_delete_function(
-                    resource,
-                    resource_model,
-                )
-                path = f"/{resource.lower()}/{{id}}"
-                response_model = resource_model
-            else:
-                raise ValueError(f"Unknown method: {method}")
-
+        for method in resource["methods"]:
             routes.append(
-                APIRoute(
-                    path=path,
-                    endpoint=endpoint_function,
-                    methods=[get_http_method(method)],
-                    summary=f"{method} {resource}",
-                    tags=[resource],
-                    operation_id=f"{method}_{resource}",
-                    response_model=response_model,
+                create_endpoint(
+                    resource_name,
+                    method,
+                    resource_model,
                 )
             )
 
