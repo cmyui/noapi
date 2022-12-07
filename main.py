14cdcc6d0cdaf0f1bc332719b50692d48c49faa3
cmyui <cmyuiosu@gmail.com>
2022-12-07 02:31:28 -0500

    fix: correct inaccuracies in configuration for resource model fields

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+from datetime import datetime
+from uuid import UUID
+from uuid import uuid4
+
+from pydantic.fields import FieldInfo
+
 from noapi import create_and_run_api
 
 if __name__ == "__main__":
@@ -16,17 +22,20 @@ if __name__ == "__main__":
             },
         ],
         "resources": [
+            # TODO: resource definitions are diverging from configuration into python code.
+            # this likely needs to be factored out into more parsing logic if we're
+            # to make to the tier of an an industry-standard technology.
             {
                 "name": "Account",
                 "table_name": "accounts",
                 "methods": ["get_many", "get_one", "post", "patch", "delete"],
                 "model": {
-                    "id": "UUID",
-                    "name": "str",
-                    "email": "str",
-                    "password": "password",
-                    "created_at": "datetime",
-                    "updated_at": "datetime",
+                    "id": (UUID, FieldInfo(default_factory=uuid4)),
+                    "name": (str, "John"),
+                    "email": (str, "john@example.com"),
+                    "password": (str, "someSecureP4assw0rd"),
+                    "created_at": (datetime, FieldInfo(default_factory=datetime.now)),
+                    "updated_at": (datetime, FieldInfo(default_factory=datetime.now)),
                 },
                 "backing_service": "mysql",  # NOTE: this is by service name
             },
@@ -35,11 +44,12 @@ if __name__ == "__main__":
                 "table_name": "accounts",
                 "methods": ["get_many", "get_one", "post", "delete"],
                 "model": {
-                    "id": "UUID",
-                    "account_id": "UUID",  # TODO: something like "$Account.id"?
-                    "expires_at": "datetime",
-                    "created_at": "datetime",
-                    "updated_at": "datetime",
+                    "id": (UUID, FieldInfo(default_factory=uuid4)),
+                    # TODO: something like "$Account.id"?
+                    "account_id": (UUID, FieldInfo(default_factory=uuid4)),
+                    "expires_at": (datetime, FieldInfo(default_factory=datetime.now)),
+                    "created_at": (datetime, FieldInfo(default_factory=datetime.now)),
+                    "updated_at": (datetime, FieldInfo(default_factory=datetime.now)),
                 },
                 "backing_service": "mysql",  # NOTE: this is by service name
             },
